05f93012ee5b85708e6769fbbf30ce1224edda3f
ramrahulpotta <44702737+ramrahulpotta@users.noreply.github.com>
2018-11-03 17:49:17 +0530

    Update model10.py

---
+++ model10.py
@@ -0,0 +1,10 @@
+a = int(input("enter a number:"))
+b = int(input("enter a number:"))
+c = input("True or False :")
+if a and b <0:
+    print(True)
+
+elif a<0 and b>0:
+        print(not True)
+else:
+    print(False)
